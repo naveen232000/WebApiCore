ca7dcae68ea8efbf4ea5cf0bb0e8554eb89ae017
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-07 14:30:46 +0530

    Add project files.

---
+++ WebApiCore/Data/EmpApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiCore.Models;
+
+namespace WebApiCore.Data
+{
+    public class EmpApiDbContext : DbContext
+    {
+        public EmpApiDbContext (DbContextOptions<EmpApiDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiCore.Models.Employee> Employee { get; set; } = default!;
+    }
+}
